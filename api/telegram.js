f34ba6800a5482dcac10539f4616b7efb76a0266
ox3tos <ox3tos@gmail.com>
2026-01-02 02:47:21 +0600

    Add POST handler for Telegram user data
    Implements a handler for POST requests to manage user data.

--- api/telegram.js
+++ api/telegram.js
@@ -1,1 +1,17 @@
-
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { code, telegram_id, username } = req.body;
+    
+    // Сохраняем в базе (пока в памяти)
+    // В реальном проекте используй базу данных
+    
+    res.json({
+      success: true,
+      user: {
+        id: telegram_id,
+        username: username,
+        token: generateToken()
+      }
+    });
+  }
+}
